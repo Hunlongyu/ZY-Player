58387784ee642157c33cd4001e68297adb5baf6f
hunlongyu <hunlongyu@gmail.com>
2019-12-24 09:33:26 +0800

    modify vue config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   pluginOptions: {
     electronBuilder: {
+      builderOptions: {
+        win: {
+          icon: './public/app.ico'
+        },
+        mac: {
+          icon: './public/app.png'
+        },
+        productName: 'ZY Player'
+      },
       chainWebpackRendererProcess: config => {
         if (process.env.NODE_ENV === 'development') {
           config.plugins.delete('prefetch')
