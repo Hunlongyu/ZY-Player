d9122fd24e8956ad8f653299e09924ca857e8917
Hunlongyu <Hunlongyu@gmail.com>
2020-01-31 05:21:46 +0800

    v0.8.6

--- src/page/setting.vue
+++ src/page/setting.vue
@@ -73,7 +73,7 @@ export default Vue.extend({
       sites: sites,
       dbSite: 0,
       opacity: 100,
-      oldVersion: 'v0.8.5',
+      oldVersion: 'v0.8.6',
       newVersion: '',
       download: false
     }
