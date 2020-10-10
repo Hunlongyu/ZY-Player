e1d0643c68de657f2bfdf23aceacb2ad393a32b2
Hunlongyu <hunlongyu@gmail.com>
2020-10-10 17:01:41 +0800

    🍔 电视直播位置移动考上

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -14,6 +14,13 @@
         </g>
       </svg>
     </span>
+    <span :class="[view === 'IPTV' ? 'active ': ''] + 'zy-svg'" @click="changeView('IPTV')">
+      <svg role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" aria-labelledby="tvIconTitle">
+        <title id="tvIconTitle">电视直播</title>
+        <polygon points="20 8 20 20 4 20 4 8"></polygon>
+        <polyline stroke-linejoin="round" points="8 4 12 7.917 16 4"></polyline>
+      </svg>
+    </span>
     <span :class="[view === 'Play' ? 'active ': ''] + 'zy-svg'" @click="changeView('Play')">
       <svg role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" aria-labelledby="playIconTitle">
         <title id="playIconTitle">播放</title>
@@ -25,13 +32,6 @@
         <title id="favouriteIconTitle">收藏</title>
         <path d="M12,21 L10.55,19.7051771 C5.4,15.1242507 2,12.1029973 2,8.39509537 C2,5.37384196 4.42,3 7.5,3 C9.24,3 10.91,3.79455041 12,5.05013624 C13.09,3.79455041 14.76,3 16.5,3 C19.58,3 22,5.37384196 22,8.39509537 C22,12.1029973 18.6,15.1242507 13.45,19.7149864 L12,21 Z"></path>
       </svg>
-    </span>
-    <span :class="[view === 'IPTV' ? 'active ': ''] + 'zy-svg'" @click="changeView('IPTV')">
-          <svg role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" aria-labelledby="tvIconTitle">
-            <title id="tvIconTitle">电视直播</title>
-            <polygon points="20 8 20 20 4 20 4 8"></polygon>
-            <polyline stroke-linejoin="round" points="8 4 12 7.917 16 4"></polyline>
-          </svg>
     </span>
     <span :class="[view === 'History' ? 'active ': ''] + 'zy-svg'" @click="changeView('History')">
           <svg role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" aria-labelledby="timeIconTitle">
