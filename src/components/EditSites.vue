dd25689dd4136c9b8b083b1e36db965419c690b6
buvta <12312540+buvta@users.noreply.github.com>
2020-10-12 06:48:28 +0800

    分组及批量设置
    加载编辑源页面有短暂间隔显示“暂无数据”

--- src/components/EditSites.vue
+++ src/components/EditSites.vue
@@ -3,7 +3,13 @@
     <div class="detail-content">
       <div class="detail-header">
         <div class="zy-select">
-            <div class="vs-placeholder vs-noAfter" @click="addSite">添加</div>
+            <div class="vs-placeholder vs-noAfter" @click="addSite">添加新源</div>
+        </div>
+        <div class="zy-select">
+            <div class="vs-placeholder vs-noAfter" @click="editGroupDialogVisible = true">分组管理</div>
+        </div>
+        <div class="zy-select">
+            <div class="vs-placeholder vs-noAfter" @click="batchDialogVisible = Boolean(multipleSelection.length)">批量设置</div>
         </div>
         <div class="zy-select">
           <div class="vs-placeholder vs-noAfter" @click="exportSites">导出</div>
@@ -12,7 +18,7 @@
           <div class="vs-placeholder vs-noAfter" @click="importSites">导入</div>
         </div>
         <div class="zy-select">
-          <div class="vs-placeholder vs-noAfter" @click="resetSitesEvent">重置</div>
+          <div class="vs-placeholder vs-noAfter" @click="resetSites">重置</div>
         </div>
         <span class="detail-close zy-svg" @click="close">
           <svg role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" aria-labelledby="closeIconTitle">
@@ -22,8 +28,76 @@
         </span>
       </div>
       <div>
-        <el-dialog :visible.sync="dialogVisible" v-if='dialogVisible' :title="dialogType==='edit'?'编辑源':'添加源'" :append-to-body="true" @close="closeDialog">
-          <el-form :model="siteInfo" ref='siteInfo' label-width="75px" label-position="left" :rules="rules">
+        <el-dialog :visible.sync="editGroupDialogVisible" v-if='editGroupDialogVisible' title="分组管理" :append-to-body="true" @close="closeDialog">
+          <el-button
+            size="small"
+            type="success"
+            @click="addGroup">新增组</el-button>
+         <el-table
+            :data="groups"
+            row-key="gid"
+            tyle="width: 100%">
+            <el-table-column
+                prop="gid"
+                label="gid"
+                width="60">
+            </el-table-column>
+            <el-table-column
+                prop="name"
+                label="组名"
+                align="center"
+                width="250">
+                <template slot-scope="scope">
+                    <el-input size="small" v-model="scope.row.name" placeholder="请输入内容" ></el-input>
+                </template>
+            </el-table-column>
+            <el-table-column
+                label="操作"
+                align="center"
+                width="250">
+                <template slot-scope="scope">
+                  <el-button @click="removeGroup(scope.row)" type="danger">删除组及属于该组的源</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <span slot="footer" class="dialog-footer">
+             <el-button @click="closeDialog">取消</el-button>
+             <el-button type="primary" @click="updateGroups">确认</el-button>
+        </span>
+       </el-dialog>
+      </div>
+      <div>
+        <el-dialog :visible.sync="batchDialogVisible" v-if='batchDialogVisible' title="批量设置" :append-to-body="true" @close="closeDialog">
+          <el-form :model="batchConfig" label-width="75px" label-position="left">
+              <el-form-item   prop='group'>
+                  <el-checkbox v-model="setGroupFlag" label="选择分组" />
+                  <el-select v-model="batchConfig.group" :disabled="!setGroupFlag" placeholder="请选择">
+                    <el-option
+                      v-for="item in groups"
+                      :key="item.gid"
+                      :label="item.name"
+                      :value="item.gid">
+                    </el-option>
+                  </el-select>
+              </el-form-item>
+              <el-form-item  prop='doubanRate'>
+                 <el-checkbox  v-model="setDoubanFlag" label="豆瓣评分" />
+                  <el-switch :disabled="!setDoubanFlag"
+                    v-model="batchConfig.doubanRate"
+                    active-color="#13ce66"
+                    inactive-color="#ff4949">
+                  </el-switch>
+              </el-form-item>
+          </el-form>
+          <span slot="footer" class="dialog-footer">
+             <el-button @click="closeDialog">取消</el-button>
+             <el-button type="primary" @click="batchSet">保存</el-button>
+          </span>
+        </el-dialog>
+      </div>
+      <div>
+        <el-dialog :visible.sync="dialogVisible" v-if='dialogVisible' :title="dialogType==='edit'?'编辑源':'添加新源'" :append-to-body="true" @close="closeDialog">
+          <el-form :model="siteInfo" label-width="75px" label-position="left" :rules="rules">
               <el-form-item label="源站名" prop='name'>
                   <el-input v-model="siteInfo.name" placeholder="请输入源站名" />
               </el-form-item>
@@ -33,30 +107,88 @@
               <el-form-item label="下载接口" prop='download'>
                   <el-input v-model="siteInfo.download" :autosize="{ minRows: 2, maxRows: 4}" type="textarea" placeholder="请输入Download接口地址，可以空着"/>
               </el-form-item>
+              <el-form-item label="所属组" prop='group'>
+                 <el-select v-model="siteInfo.group"  placeholder="请选择">
+                    <el-option default-first-option
+                      v-for="item in groups"
+                      :key="item.gid"
+                      :label="item.name"
+                      :value="item.gid">
+                    </el-option>
+                  </el-select>
+              </el-form-item>
+              <el-form-item label="豆瓣评分" prop='doubanRate'>
+                  <el-switch
+                    v-model="siteInfo.doubanRate"
+                    active-color="#13ce66"
+                    inactive-color="#ff4949">
+                  </el-switch>
+              </el-form-item>
           </el-form>
           <span slot="footer" class="dialog-footer">
              <el-button @click="closeDialog">取消</el-button>
-             <el-button type="primary" @click="addOrEditSite">保存</el-button>
+             <el-button type="primary" @click="addOrEditSite">确定</el-button>
           </span>
         </el-dialog>
       </div>
       <div class="detail-body zy-scroll">
         <div class="zy-table">
-          <div class="tBody zy-scroll">
-            <ul>
-              <draggable v-model="sites" @change="listUpdatedEvent">
-                <transition-group>
-                  <li v-for="(i, j) in sites" :key="j">
-                    <span class="name">{{i.name}}</span>
-                    <span class="operate">
-                      <span class="btn" @click.stop="moveSiteToTop(i)">置顶</span>
-                      <span class="btn" @click.stop="editSite(i)">编辑</span>
-                      <span class="btn" @click.stop="removeEvent(i)">删除</span>
-                    </span>
-                  </li>
-                </transition-group>
-              </draggable>
-            </ul>
+          <div class="tBody zy-scroll" >
+            <el-table  @selection-change="handleSelectionChange"
+              :data="sites"
+              v-loading="!sites.length"
+              row-key="id"
+              style="width: 100%">
+              <el-table-column
+                type="selection"
+                width="55">
+              </el-table-column>
+              <el-table-column
+                prop="name"
+                label="源站名"
+                width="200">
+              </el-table-column>
+              <el-table-column
+                prop="group"
+                label="所属组"
+                header-align="center"
+                width="200">
+                <template slot-scope="scope">
+                  <el-select v-model="scope.row.group" placeholder="请选择" @change="changeSite(scope.row)">
+                    <el-option
+                      v-for="item in groups"
+                      :key="item.gid"
+                      :label="item.name"
+                      :value="item.gid">
+                    </el-option>
+                  </el-select>
+                </template>
+              </el-table-column>
+              <el-table-column
+                prop="doubanRate"
+                label="豆瓣评分"
+                align="center"
+                width="100">
+                <template slot-scope="scope">
+                  <el-switch @change="changeSite(scope.row)"
+                    v-model="scope.row.doubanRate"
+                    active-color="#13ce66"
+                    inactive-color="#ff4949">
+                  </el-switch>
+                </template>
+              </el-table-column>
+              <el-table-column
+                fixed="right"
+                label="操作"
+                align="center"
+                width="150">
+                <template slot-scope="scope">
+                  <el-button @click="moveSiteToTop(scope.row)" type="text">置顶</el-button>
+                  <el-button @click="editSite(scope.row)" type="text">编辑</el-button>
+                  <el-button @click="removeSite(scope.row)" type="text">删除</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
           </div>
         </div>
       </div>
@@ -65,10 +197,10 @@
 </template>
 <script>
 import { mapMutations } from 'vuex'
-import { sites, setting } from '../lib/dexie'
-import draggable from 'vuedraggable'
+import { sites, setting, groups } from '../lib/dexie'
 import { remote } from 'electron'
-import { sites as defaultSites } from '../lib/dexie/initData'
+import { sites as defaultSites, groups as defaultGroups } from '../lib/dexie/initData'
+import Sortable from 'sortablejs'
 import fs from 'fs'
 import Vue from 'vue'
 import ElementUI from 'element-ui'
@@ -82,10 +214,22 @@ export default {
       sites: [],
       dialogType: 'new',
       dialogVisible: false,
+      batchDialogVisible: false,
+      editGroupDialogVisible: false,
+      setGroupFlag: true,
+      setDoubanFlag: true,
       siteInfo: {
         name: '',
+        group: '',
+        doubanRate: '',
         api: '',
         download: ''
+      },
+      tempGroups: [],
+      groups: [],
+      batchConfig: {
+        group: '',
+        doubanRate: ''
       },
       rules: {
         name: [
@@ -97,11 +241,9 @@ export default {
         download: [
           { required: false, trigger: 'blur' }
         ]
-      }
+      },
+      multipleSelection: []
     }
-  },
-  components: {
-    draggable
   },
   computed: {
     setting: {
@@ -126,9 +268,68 @@ export default {
     close () {
       this.editSites.show = false
     },
+    closeDialog () {
+      this.dialogVisible = false
+      this.batchDialogVisible = false
+      this.editGroupDialogVisible = false
+      if (this.tempGroups.length) this.groups = [...this.tempGroups]
+      this.tempGroups = []
+      this.getSites()
+    },
+    handleSelectionChange (val) {
+      this.multipleSelection = val
+    },
+    addGroup () {
+      if (!this.tempGroups.length) this.tempGroups = [...this.groups]
+      var newGroup = {
+        gid: Math.max.apply(null, this.groups.map(g => g.gid)) + 1,
+        name: ''
+      }
+      this.groups.push(newGroup)
+      console.log(this.tempGroups === this.groups)
+    },
+    removeGroup (group) {
+      if (!this.tempGroups.length) this.tempGroups = [...this.groups]
+      const index = this.tempGroups.indexOf(group)
+      this.groups.splice(index, 1)
+    },
+    updateGroups () {
+      if (!this.tempGroups.length) { this.editGroupDialogVisible = false; return }
+      const gids = this.tempGroups.map(group => group.gid)
+      this.sites = this.sites.filter(site => gids.includes(site.group))
+      sites.clear()
+      this.sites.forEach(site => sites.add(site))
+      groups.clear()
+      this.groups.forEach(group => groups.add(group))
+
+      this.editGroupDialogVisible = false
+      this.tempGroups = []
+      this.$message.success('调整分组相关操作已完成！')
+    },
+    batchSet () {
+      this.multipleSelection.forEach(site => {
+        sites.remove(site.id)
+        if (this.setGroupFlag) site.group = this.batchConfig.group
+        if (this.setDoubanFlag) site.doubanRate = this.batchConfig.doubanRate
+        sites.add(site)
+      })
+      this.batchDialogVisible = false
+      this.$message.success('批量设置完成！')
+      this.getSites()
+    },
+    changeSite (site) {
+      sites.remove(site.id)
+      sites.add(site)
+    },
     getSites () {
       sites.all().then(res => {
+        res.forEach(site => { if (site.group === undefined) site.group = 0; if (site.doubanRate === undefined) site.doubanRate = true })
         this.sites = res
+      })
+    },
+    getGroups () {
+      groups.all().then(res => {
+        this.groups = res
       })
     },
     addSite () {
@@ -136,6 +337,8 @@ export default {
       this.dialogVisible = true
       this.siteInfo = {
         name: '',
+        group: '',
+        doubanRate: '',
         api: '',
         download: ''
       }
@@ -145,20 +348,17 @@ export default {
       this.dialogVisible = true
       this.siteInfo = siteInfo
     },
-    closeDialog () {
-      this.dialogVisible = false
-      this.getSites()
-    },
-    removeEvent (e) {
-      sites.remove(e.id).then(res => {
+    removeSite (site) {
+      sites.remove(site.id).then(res => {
         this.getSites()
       }).catch(err => {
         this.$message.warning('删除源失败, 错误信息: ' + err)
       })
     },
-    listUpdatedEvent () {
+    moveSiteToTop (site) {
+      this.sites.splice(site.id - 1, 1)
+      this.sites.splice(0, 0, site)
       sites.clear().then(res1 => {
-        // 重新排序
         var id = 1
         this.sites.forEach(element => {
           element.id = id
@@ -177,6 +377,8 @@ export default {
         key: this.dialogType === 'edit' ? this.siteInfo.key : randomstring.generate(6),
         id: this.dialogType === 'edit' ? this.siteInfo.id : this.sites[this.sites.length - 1].id + 1,
         name: this.siteInfo.name,
+        group: this.siteInfo.group,
+        doubanRate: this.siteInfo.doubanRate,
         api: this.siteInfo.api,
         download: this.siteInfo.download
       }
@@ -184,6 +386,8 @@ export default {
       sites.add(doc).then(res => {
         this.siteInfo = {
           name: '',
+          group: '',
+          doubanRate: '',
           api: '',
           download: ''
         }
@@ -241,28 +445,43 @@ export default {
         }
       })
     },
-    resetSitesEvent () {
-      this.resetSites(defaultSites)
+    resetSites () {
+      sites.clear()
+      sites.bulkAdd(defaultSites).then(e => {
+        this.getSites()
+      })
+      groups.clear()
+      groups.bulkAdd(defaultGroups).then(e => {
+        this.getGroups()
+      })
       this.$message.success('重置源成功')
     },
-    moveSiteToTop (i) {
-      this.sites.sort(function (x, y) { return x.key === i.key ? -1 : y.key === i.key ? 1 : 0 })
-      this.resetSites(this.sites)
-    },
-    resetId (inArray) {
-      var id = 1
-      inArray.forEach(ele => {
-        ele.id = id
-        id += 1
+    rowDrop () {
+      const tbody = document.querySelector('.el-table__body-wrapper tbody')
+      const _this = this
+      Sortable.create(tbody, {
+        onEnd ({ newIndex, oldIndex }) {
+          const currRow = _this.sites.splice(oldIndex, 1)[0]
+          _this.sites.splice(newIndex, 0, currRow)
+          sites.clear().then(res1 => {
+            // 重新排序
+            var id = 1
+            _this.sites.forEach(element => {
+              element.id = id
+              sites.add(element)
+              id += 1
+            })
+          })
+        }
       })
-    },
-    resetSites (newSites) {
-      this.resetId(newSites)
-      sites.clear().then(sites.bulkAdd(newSites).then(this.getSites()))
     }
+  },
+  mounted () {
+    this.rowDrop()
   },
   created () {
     this.getSites()
+    this.getGroups()
   }
 }
 
