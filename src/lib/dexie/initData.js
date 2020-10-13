296abff511a8a58c59389cda8d725bb8d2eb959b
buvta <12312540+buvta@users.noreply.github.com>
2020-10-14 00:24:05 +0800

    同步初始化数据

--- src/lib/dexie/initData.js
+++ src/lib/dexie/initData.js
@@ -17,6 +17,8 @@ const sites = [
     id: 1,
     key: 'okzy',
     name: 'OK 资源网',
+    group: 0,
+    doubanRate: true,
     api: 'http://cj.okzy.tv/inc/api.php',
     download: 'http://cj.okzy.tv/inc/apidown.php'
   },
@@ -24,6 +26,8 @@ const sites = [
     id: 2,
     key: 'zuidazy',
     name: '最大资源网',
+    group: 0,
+    doubanRate: true,
     api: 'http://www.zdziyuan.com/inc/api.php',
     download: 'http://www.zdziyuan.com/inc/apidown.php'
   },
@@ -31,6 +35,8 @@ const sites = [
     id: 3,
     key: 'doubanzy',
     name: '豆瓣电影资源',
+    group: 0,
+    doubanRate: true,
     api: 'http://v.1988cj.com/inc/api.php',
     download: 'http://v.1988cj.com/inc/apidown.php'
   },
@@ -38,6 +44,8 @@ const sites = [
     id: 4,
     key: '135zy',
     name: '135 资源网',
+    group: 0,
+    doubanRate: true,
     api: 'http://cj.zycjw1.com/inc/api.php',
     download: 'http://cj.zycjw1.com/inc/apidown.php'
   },
@@ -45,6 +53,8 @@ const sites = [
     id: 5,
     key: 'kuyunzy',
     name: '酷云资源',
+    group: 0,
+    doubanRate: true,
     api: 'http://caiji.kuyun98.com/inc/ldg_api.php',
     download: 'http://caiji.kuyun98.com/inc/apidown.php'
   },
@@ -52,6 +62,8 @@ const sites = [
     id: 6,
     key: 'mgtvzy',
     name: '芒果 TV 资源网',
+    group: 0,
+    doubanRate: true,
     api: 'https://api.shijiapi.com/api.php/provide/vod/at/xml/',
     download: ''
   },
@@ -59,6 +71,8 @@ const sites = [
     id: 7,
     key: 'subo988',
     name: '速播资源站',
+    group: 0,
+    doubanRate: true,
     api: 'https://www.subo988.com/inc/api.php',
     download: ''
   },
@@ -66,6 +80,8 @@ const sites = [
     id: 8,
     key: '209zy',
     name: '209 资源',
+    group: 0,
+    doubanRate: true,
     api: 'http://cj.1156zy.com/inc/api.php',
     download: ''
   },
@@ -73,6 +89,8 @@ const sites = [
     id: 9,
     key: 'zuixinzy',
     name: '最新资源',
+    group: 0,
+    doubanRate: true,
     api: 'http://api.zuixinapi.com/inc/api.php',
     download: ''
   },
@@ -80,6 +98,8 @@ const sites = [
     id: 10,
     key: 'kubozy',
     name: '酷播资源',
+    group: 0,
+    doubanRate: true,
     api: 'http://api.kbzyapi.com/inc/api.php',
     download: ''
   },
@@ -87,6 +107,8 @@ const sites = [
     id: 11,
     key: 'yongjiuzy',
     name: '永久资源',
+    group: 0,
+    doubanRate: true,
     api: 'http://cj.yongjiuzyw.com/inc/api.php',
     download: ''
   },
@@ -94,6 +116,8 @@ const sites = [
     id: 12,
     key: '123ku',
     name: '123 资源',
+    group: 0,
+    doubanRate: true,
     api: 'http://cj.123ku2.com:12315/inc/api.php',
     download: ''
   },
@@ -101,6 +125,8 @@ const sites = [
     id: 13,
     key: '88zyw',
     name: '88 影视资源站',
+    group: 0,
+    doubanRate: true,
     api: 'http://www.88zyw.net/inc/api.php',
     download: ''
   },
@@ -108,6 +134,8 @@ const sites = [
     id: 14,
     key: 'wolongzy',
     name: '卧龙资源',
+    group: 0,
+    doubanRate: true,
     api: 'http://cj.wlzy.tv/inc/api_mac.php',
     download: ''
   },
@@ -115,6 +143,8 @@ const sites = [
     id: 15,
     key: 'mahuazy',
     name: '麻花资源',
+    group: 0,
+    doubanRate: true,
     api: 'https://www.mhapi123.com/inc/api.php',
     download: ''
   },
@@ -122,6 +152,8 @@ const sites = [
     id: 16,
     key: 'kkzy',
     name: '快快资源',
+    group: 0,
+    doubanRate: true,
     api: 'https://api.kkzy.tv/inc/api.php',
     download: ''
   },
@@ -129,6 +161,8 @@ const sites = [
     id: 17,
     key: '158zy',
     name: '壹伍捌资源网',
+    group: 0,
+    doubanRate: true,
     api: 'http://cj.158zyz.net:158/inc/api.php',
     download: ''
   },
@@ -136,6 +170,8 @@ const sites = [
     id: 18,
     key: 'rrzy',
     name: '人人资源',
+    group: 0,
+    doubanRate: true,
     api: 'https://www.rrzyw.cc/api.php/provide/vod/from/rrm3u8/at/xml/',
     download: ''
   },
@@ -143,6 +179,8 @@ const sites = [
     id: 19,
     key: 'mokazy',
     name: '魔卡资源网',
+    group: 0,
+    doubanRate: true,
     api: 'https://cj.heiyap.com/api.php/provide/vod/at/xml/',
     download: ''
   },
@@ -150,6 +188,8 @@ const sites = [
     id: 20,
     key: 'kyzy',
     name: '快影资源站',
+    group: 0,
+    doubanRate: true,
     api: 'https://www.kyzy.tv/api.php/kyyun/vod/at/xml/',
     download: ''
   },
@@ -157,6 +197,8 @@ const sites = [
     id: 21,
     key: 'solezy',
     name: '搜乐资源网',
+    group: 0,
+    doubanRate: true,
     api: 'https://www.caijizy.vip/api.php/provide/vod/at/xml/',
     download: ''
   },
@@ -164,6 +206,8 @@ const sites = [
     id: 22,
     key: 'bbkdj',
     name: '步步高顶尖资源网',
+    group: 0,
+    doubanRate: true,
     api: 'http://api.bbkdj.com/api',
     download: ''
   },
@@ -171,6 +215,8 @@ const sites = [
     id: 23,
     key: '1886zy',
     name: '1886 资源',
+    group: 0,
+    doubanRate: true,
     api: 'http://cj.1886zy.co/inc/api.php',
     download: ''
   },
@@ -178,6 +224,8 @@ const sites = [
     id: 24,
     key: 'mbo',
     name: '秒播资源',
+    group: 0,
+    doubanRate: true,
     api: 'http://caiji.mb77.vip/inc/api.php',
     download: ''
   },
@@ -185,6 +233,8 @@ const sites = [
     id: 25,
     key: '605zy',
     name: '605资源',
+    group: 0,
+    doubanRate: true,
     api: 'http://www.605zy.net/inc/seacmsapi.php',
     download: ''
   }
