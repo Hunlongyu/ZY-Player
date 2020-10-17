dbf85cbc4299110aae229bc8d94c8dae457a2337
buvta <deepin@localhost.localdomain>
2020-10-17 11:29:16 +0800

    引入el-table列自适应方案

--- src/lib/element/index.js
+++ src/lib/element/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import { Message, Table, TableColumn } from 'element-ui'
+import Plugin from 'v-fit-columns'
+
 Vue.use(Table)
 Vue.use(TableColumn)
+Vue.use(Plugin)
 Vue.prototype.$message = Message
