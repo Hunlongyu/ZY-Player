5c7dd99f3cc568eb55edf73e6fd7c4ffddda89c2
buvta <12312540+buvta@users.noreply.github.com>
2020-10-21 16:50:30 +0800

    调整数据库保存新搜索配置

--- src/lib/dexie/dexie.js
+++ src/lib/dexie/dexie.js
@@ -1,25 +1,29 @@
 import Dexie from 'dexie'
-import { setting, sites, localKey, iptv } from './initData'
+import { setting, sites, groups, localKey, iptv, channelGroups } from './initData'
 
 const db = new Dexie('zy')
 
 db.version(3).stores({
   search: '++id, keywords',
   iptvSearch: '++id, keywords',
-  setting: 'id, theme, site, shortcut, view, externalPlayer, searchAllSites, excludeRootClasses, excludeR18Films, forwardTimeInSec',
+  setting: 'id, theme, site, shortcut, view, externalPlayer, searchGroup, excludeRootClasses, excludeR18Films, forwardTimeInSec',
   shortcut: 'name, key, desc',
   star: '++id, site, ids, name, type, year, index',
   sites: '++id, key, name, json, xml, down, level',
+  groups: '++gid, name, doubanRate',
   history: '++id, site, ids, name, type, year, index, time',
   mini: 'id, site, ids, name, index, time',
-  iptv: '++id, name, url, group'
+  iptv: '++id, name, url, gid',
+  channelGroups: '++gid, name'
 })
 
 db.on('populate', () => {
   db.setting.bulkAdd(setting)
   db.sites.bulkAdd(sites)
+  db.groups.bulkAdd(groups)
   db.shortcut.bulkAdd(localKey)
   db.iptv.bulkAdd(iptv)
+  db.channelGroups.bulkAdd(channelGroups)
 })
 
 db.open()
