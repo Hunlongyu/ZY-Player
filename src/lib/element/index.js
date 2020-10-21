b68525213a3e52b9b975144d5a1c2634a14720d3
haiyangcui <cuiocean@gmail.com>
2020-10-21 16:07:37 +0200

    引入需要的element-ui的组件

--- src/lib/element/index.js
+++ src/lib/element/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
-import { Message, Button, Table, TableColumn, Tag, Input } from 'element-ui'
+import { Message, Button, Table, TableColumn, Tag, Input, Dialog, Form, FormItem } from 'element-ui'
 Vue.use(Button)
 Vue.use(Table)
 Vue.use(TableColumn)
 Vue.use(Tag)
 Vue.use(Input)
+Vue.use(Dialog)
+Vue.use(Form)
+Vue.use(FormItem)
 Vue.prototype.$message = Message
