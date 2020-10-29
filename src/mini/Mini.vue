3044bfcbc4909f82475aeee1236fa0f8e78caac6
Hunlongyu <hunlongyu@gmail.com>
2020-10-29 16:59:25 +0800

    🏈 mini 界面优化

--- src/mini/Mini.vue
+++ src/mini/Mini.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mini">
-    <div class="top">
+    <div class="header">
       <div class="left">
         <span class="title">
           <span v-if="m3u8Arr.length > 1">『第 {{(video.index + 1)}} 集』</span>{{name}}
@@ -22,7 +22,7 @@
         <span class="progress" v-show="progress > 0">播放进度: {{progress}}%</span>
       </div>
       <div class="right">
-        <span class="top" @click="frameClickEvent('top')" title="置顶">
+        <span class="topping" @click="frameClickEvent('top')" title="置顶">
           <svg t="1595919317571" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1188" style="width:10px;height:14px"><path d="M43.072 974.72l380.864-301.952 151.936 161.6c0 0 63.424 17.28 67.328-30.72l-3.904-163.584 225.088-259.648 98.048-5.696c0 0 76.928-15.488 21.184-82.752l-275.072-276.928c0 0-74.944-9.6-69.248 59.584l0 75.008L383.552 367.104 225.856 376.64c0 0-57.728 19.2-36.608 69.248l148.16 146.176L43.072 974.72 43.072 974.72z" p-id="1189" :fill="isAlwaysOnTop ? '#555555' : '#ffffff'"></path></svg>
         </span>
         <span class="min" @click="frameClickEvent('min')" title="最小化">
@@ -31,12 +31,12 @@
         <span class="max" @click="frameClickEvent('max')" title="最大化">
           <svg t="1595917343956" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1540" style="width:8px;height:14px"><path d="M416 416 64.064 416C28.448 416 0 444.64 0 479.936L0 544.064C0 579.264 28.672 608 64.064 608L416 608 416 959.936C416 995.552 444.64 1024 479.936 1024L544.064 1024C579.264 1024 608 995.328 608 959.936L608 608 959.936 608C995.552 608 1024 579.36 1024 544.064L1024 479.936C1024 444.736 995.328 416 959.936 416L608 416 608 64.064C608 28.448 579.36 0 544.064 0L479.936 0C444.736 0 416 28.672 416 64.064L416 416Z" p-id="1541" fill="#ffffff"></path></svg>
         </span>
-        <span class="close" @click="frameClickEvent('close')" title="关闭">
+        <span class="close" @click="frameClickEvent('close')" title="退出精简模式">
           <svg t="1595917372551" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1685" style="width:8px;height:14px"><path d="M511.968 376.224 796.096 92.096C833.536 54.624 894.4 54.624 931.84 92.096 969.312 129.568 969.312 190.4 931.84 227.872L647.744 512 931.84 796.096C969.312 833.568 969.312 894.4 931.84 931.872 894.4 969.344 833.536 969.344 796.096 931.872L511.968 647.744 227.84 931.872C190.4 969.344 129.536 969.344 92.096 931.872 54.624 894.4 54.624 833.568 92.096 796.096L376.224 512 92.096 227.872C54.624 190.4 54.624 129.568 92.096 92.096 129.536 54.624 190.4 54.624 227.84 92.096L511.968 376.224Z" p-id="1686" fill="#ffffff"></path></svg>
         </span>
       </div>
     </div>
-    <div class="bottom">
+    <div class="footer">
       <div id="xg"></div>
     </div>
   </div>
@@ -419,9 +419,10 @@ html,body{
   justify-content: center;
   align-items: flex-start;
   flex-direction: column;
-  .top{
+  .header{
     width: 100%;
     height: 30px;
+    min-height: 30px;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -455,22 +456,27 @@ html,body{
       }
     }
     .right{
-      width: 80px;
+      width: 100px;
       height: 100%;
       display: flex;
       justify-content: flex-end;
       align-items: center;
       span{
         -webkit-app-region: no-drag;
-        display: inline-block;
-        width: 14px;
-        height: 14px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 15px;
+        height: 15px;
         text-align: center;
-        line-height: 14px;
+        line-height: 15px;
         border-radius: 50%;
         margin-right: 10px;
         cursor: pointer;
         opacity: 0.4;
+        &.topping{
+          background-color: #f3bab7;
+        }
         &.min{
           background-color: #32dc36;
         }
@@ -479,9 +485,6 @@ html,body{
         }
         &.close{
           background-color: #ff5f56;
-        }
-        &.top{
-          background-color: #f3bab7;
         }
         &:hover{
           animation: heartbeat 3s ease-in-out infinite both;
@@ -513,7 +516,7 @@ html,body{
       }
     }
   }
-  .bottom{
+  .footer{
     width: 100%;
     flex: 1;
     .xgplayer-start{
