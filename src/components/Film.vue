9a678219841f1d8cd0443395aa476dba6590d08f
haiyangcui <cuiocean@gmail.com>
2020-11-12 23:35:21 +0100

    移除无用log

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -450,7 +450,6 @@ export default {
       const key = this.site.key
       const type = this.type.tid
       const page = this.pagecount
-      console.log(key, type, page, 'infiniteHandler') // TODO: 初次进入会重复请求两次, 导致数据重复, 代码报错.
       this.statusText = ' '
       if (key && page < 1) { // OK资源前几类硬是去不掉
         $state.complete()
