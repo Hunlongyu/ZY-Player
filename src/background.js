1fefc792fc330874d6d2eb54e1ea1ef1efb95934
buvta <12312540+buvta@users.noreply.github.com>
2020-12-29 16:00:45 +0800

    修改请求referer

--- src/background.js
+++ src/background.js
@@ -5,6 +5,7 @@ import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 import { initUpdater } from './lib/update/update'
 const isDevelopment = process.env.NODE_ENV !== 'production'
+// const log = require('electron-log') // 用于调试主程序
 
 app.commandLine.appendSwitch('disable-features', 'OutOfBlinkCors') // 允许跨域
 app.commandLine.appendSwitch('--ignore-certificate-errors', 'true') // 忽略证书相关错误
@@ -34,6 +35,23 @@ function createWindow () {
     createProtocol('app')
     win.loadURL('app://./index.html')
   }
+
+  // 修改request headers
+  // Sec-Fetch下禁止修改，浏览器自动加上请求头 https://www.cnblogs.com/fulu/p/13879080.html 暂时先用index.html的meta referer policy替代
+  const filter = {
+      urls: ['http://*/*', 'http://*/*']
+  }
+  win.webContents.session.webRequest.onBeforeSendHeaders(filter, (details, callback) => {
+    const url = new URL(details.url)
+    details.requestHeaders.Origin = url.origin
+    if (!details.url.includes('//localhost') && details.requestHeaders.Referer && details.requestHeaders.Referer.includes('//localhost')) {
+      details.requestHeaders.Referer = url.origin
+    }
+    callback({
+      cancel: false,
+      requestHeaders: details.requestHeaders
+    })
+  })
 
   initUpdater(win)
 
