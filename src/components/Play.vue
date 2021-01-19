97aea7b98dd3f2424701bd668e6d8a53e68c77bb
buvta <12312540+buvta@users.noreply.github.com>
2021-01-19 16:21:06 +0800

    修复因分隔符"$"改动引入的bug

--- src/components/Play.vue
+++ src/components/Play.vue
@@ -641,7 +641,7 @@ export default {
         }
         this.right.list = playlist
         const url = playlist[index].includes('$') ? playlist[index].split('$')[1] : playlist[index]
-        if (playlist.every(e => e.endsWith('.m3u8') || e.split('$')[1].endsWith('.m3u8'))) this.exportablePlaylist = true
+        if (playlist.every(e => e.includes('$') ? e.split('$')[1].endsWith('.m3u8') : e.endsWith('.m3u8'))) this.exportablePlaylist = true
         if (!url.endsWith('.m3u8') && !url.endsWith('.mp4')) {
           const currentSite = await sites.find({ key: this.video.key })
           this.$message.info('即将调用解析接口播放，请等待...')
